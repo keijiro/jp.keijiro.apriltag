f888c29ffe0ac0446ad3c92a146408aa1e7d2885
adrian-babilinski <ababilinski@gmail.com>
2021-05-12 23:00:25 -0400

    add flipped image processor
    (cherry picked from commit f90af2804c10b47bf97b1c78a4a344fe9568e426)

--- Packages/jp.keijiro.apriltag/Runtime/Unity/Internal/ImageConverter.cs
+++ Packages/jp.keijiro.apriltag/Runtime/Unity/Internal/ImageConverter.cs
@@ -1,5 +1,7 @@
 using System;
 using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Color32 = UnityEngine.Color32;
 using ImageU8 = AprilTag.Interop.ImageU8;
 
@@ -11,17 +13,23 @@ namespace AprilTag {
 [BurstCompile]
 static class ImageConverter
 {
-    public unsafe static void
-      Convert(ReadOnlySpan<Color32> data, ImageU8 image)
+    public unsafe static void Convert(ReadOnlySpan<Color32> data, ImageU8 image)
     {
         fixed (Color32* src = &data.GetPinnableReference())
             fixed (byte* dst = &image.Buffer.GetPinnableReference())
                 BurstConvert(src, dst, image.Width, image.Height, image.Stride);
     }
 
-    [BurstCompile]
-    unsafe static void BurstConvert
-      (Color32* src, byte* dst, int width, int height, int stride)
+
+    public unsafe static void ConvertFlipped(ReadOnlySpan<Color32> data, ImageU8 image)
+    {
+        fixed (Color32* src = &data.GetPinnableReference())
+        fixed (byte* dst = &image.Buffer.GetPinnableReference())
+        BurstConvertFlipped(src, dst, image.Width, image.Height, image.Stride);
+    }
+
+[BurstCompile]
+    unsafe static void BurstConvert (Color32* src, byte* dst, int width, int height, int stride)
     {
         var offs_src = 0;
         var offs_dst = stride * (height - 1);
@@ -35,6 +43,21 @@ static class ImageConverter
             offs_dst -= stride;
         }
     }
+    unsafe static void BurstConvertFlipped (Color32* src, byte* dst, int width, int height, int stride)
+    {
+        var offs_src = 0;
+        var offs_dst = 0;
+
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+                dst[offs_dst + x] = src[offs_src + x].g;
+
+            offs_src += width;
+            offs_dst += stride;
+        }
+    }
+
 }
 
 } // namespace AprilTag
